5f6b12cb0b6acdec032e6d891ff4b23d01fcc968
KarpoorMaaz <159101896+KarpoorMaaz@users.noreply.github.com>
2024-02-22 17:07:09 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! MOHAMMED MAAZ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "! Nice to meet you.");
+        scanner.close();
     }
 }
